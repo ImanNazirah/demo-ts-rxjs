ce38e101e87ac418fe5f6e9256dc0b121f203a4f
ImanNazirah <nurimannazirah95@gmail.com>
2024-08-10 13:34:05 +0800

    format document

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import * as dotenv from 'dotenv';
-import { GroupedObservable, from, of} from 'rxjs';
+import { GroupedObservable, of } from 'rxjs';
 import { Post } from './api/Post';
 import { User } from './api/User';
 import { PostData, PostOutput } from './model/PostOutput';
-import { PostService} from './service/PostService';
+import { PostService } from './service/PostService';
 import { UserService } from './service/UserService';
 import { tap, map, groupBy, mergeMap, toArray, switchMap } from 'rxjs/operators';
 import axios from 'axios';
@@ -16,10 +16,11 @@ async function main() {
     const postService = new PostService(axios);
     const userService = new UserService(axios);
     let transformedPost: PostOutput[] = [];
-    let sortUserByName : User[];
-    let sortUserByCity : User[];
-    let cityList:string[];
-    let filterUserCity : User[];
+    let sortUserByName: User[];
+    let sortUserByCity: User[];
+    let cityList: string[];
+    let filterUserCity: User[];
+
     try {
 
         const processSortByName = (x: User[]) => {
@@ -38,7 +39,7 @@ async function main() {
             );
         };
 
-        const processCityList =  (x: User[]) => {
+        const processCityList = (x: User[]) => {
             return of(x).pipe(
                 map((users: User[]) => {
                     const cities = users.map(y => y.address.city);
@@ -48,14 +49,14 @@ async function main() {
             );
         };
 
-        const processFilterUserCity =  (x: User[]) => {
+        const processFilterUserCity = (x: User[]) => {
             return of(x).pipe(
                 map((users: User[]) => {
                     return users.filter(user => user.address.city.toLowerCase().includes('south'));
                 })
             );
         };
-          
+
         // Fetch data and then process it
         userService.getUsers().subscribe({
 
@@ -64,15 +65,15 @@ async function main() {
                 console.log('Fetched Users From API : ', x);
 
                 processSortByName(x).subscribe({
-                    next: transformedData => {
+                    next: (transformedData: User[]) => {
                         sortUserByName = transformedData;
                         console.log('Processed processSortByName:', JSON.stringify(sortUserByName, null, 2))
                     },
                     error: err => console.error('Error in Processing processSortByName:', err)
                 });
-            
+
                 processSortByCity(x).subscribe({
-                    next: transformedData => {
+                    next: (transformedData: User[]) => {
                         sortUserByCity = transformedData;
                         console.log('Processed processSortByCity:', JSON.stringify(sortUserByCity, null, 2))
                     },
@@ -80,7 +81,7 @@ async function main() {
                 });
 
                 processCityList(x).subscribe({
-                    next: transformedData => {
+                    next: (transformedData: string[]) => {
                         cityList = transformedData;
                         console.log('Processed processCityList:', JSON.stringify(cityList, null, 2))
                     },
@@ -88,7 +89,7 @@ async function main() {
                 });
 
                 processFilterUserCity(x).subscribe({
-                    next: transformedData => {
+                    next: (transformedData: User[]) => {
                         filterUserCity = transformedData;
                         console.log('Processed processFilterUserCity:', JSON.stringify(filterUserCity, null, 2))
                     },
@@ -98,34 +99,34 @@ async function main() {
             },
             error: err => console.error('Error in Fetching x:', err)
         });
-       
+
         //Group based on userId then display different type
         postService.getPosts().pipe(
             tap((x: Post[]) => console.log('Fetched Post From API:', x)),
             switchMap((posts: Post[]) => posts),
             // tap((x: Post) => console.log('Checking process switchMap:', x)),
-            groupBy((x:Post) => x.userId),
+            groupBy((x: Post) => x.userId),
             // tap((group: GroupedObservable<number, Post>) => group.subscribe((item: Post) => console.log('Checking process groupBy:', item))),
             mergeMap((y: GroupedObservable<number, Post>) => y.pipe(toArray())),
-            map((x: Post[]) =>{
+            map((x: Post[]) => {
 
-                const transformedPosts = x.map((post: Post) => 
+                const transformedPosts = x.map((post: Post) =>
                     new PostData(post.id, post.title, post.body)
                 );
 
                 const dataDisplay = new PostOutput();
                 dataDisplay.userId = x[0].userId;
                 dataDisplay.data = transformedPosts;
-                
+
                 return dataDisplay;
             }),
-            toArray() 
+            toArray()
         )
-        .subscribe((x: PostOutput[])=>{
-            transformedPost = x;
-            console.log('Processed Post:', JSON.stringify(transformedPost, null, 2));
-        });
-        
+            .subscribe((x: PostOutput[]) => {
+                transformedPost = x;
+                console.log('Processed Post:', JSON.stringify(transformedPost, null, 2));
+            });
+
     } catch (error) {
         console.error('Error in main function:', error);
     }
